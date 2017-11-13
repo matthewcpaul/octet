5ca59123971fa566dd84339c7f52f32b39a84ce8
Matthew Paul <mclydep@gmail.com>
2017-11-13 16:47:35 -0500

    :tada: Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,74 @@
+var gulp         = require('gulp');
+var sass         = require('gulp-sass');
+var scsslint     = require('gulp-scss-lint');
+var nano         = require('gulp-cssnano');
+var shell        = require('gulp-shell');
+var sourcemaps   = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync  = require('browser-sync').create();
+var deploy       = require('gulp-gh-pages');
+var svgstore     = require('gulp-svgstore');
+var rename       = require('gulp-rename');
+
+// Build incrementally with _config.yml + _local_config.yml for local development
+gulp.task('local-build', shell.task(['bundle exec jekyll build --config _config.yml,_local_config.yml']));
+
+// Build incrementally with _config.yml for production
+gulp.task('production-build', shell.task(['bundle exec jekyll build --config _config.yml']));
+
+// Compile SCSS into CSS, sourcemaps, autoprefixer, cssnano + auto-inject into browsers
+gulp.task('sass', function() {
+  return gulp.src(['_styles/scss/style.scss'])
+  .pipe(sass({
+    includePaths: [
+      'node_modules/ibm-design-colors',
+    ]
+  }))
+  .pipe(autoprefixer())
+  .pipe(nano({discardComments: {removeAll: true}}))
+  .pipe(gulp.dest('_site/assets/css'))
+  .pipe(browserSync.stream());
+});
+
+// Create icon-store.svg
+gulp.task('icons', function() {
+  return gulp.src(['node_modules/ibm-design-icons/dist/svg/**/*.svg', 'images/**/*.svg'])
+    .pipe(svgstore())
+    .pipe(rename('icon-store.svg'))
+    .pipe(gulp.dest('_site/images/'));
+});
+
+// Pipe _fonts to _site
+gulp.task('fonts', function() {
+  return gulp.src('_fonts/*.*')
+    .pipe(gulp.dest('_site/assets/fonts'));
+});
+
+// Start a local server with browser-sync + watch for changes
+gulp.task('serve', function() {
+  browserSync.init({
+    server: { baseDir: '_site/' }
+  });
+
+  gulp.watch('_styles/scss/**/*.scss', gulp.series('local-build', 'sass', 'icons'));
+  gulp.watch(['_includes/*.html', '_layouts/*.html', 'index.md', '**/*.md'], gulp.series('local-build', 'sass', 'icons'));
+  gulp.watch('_site/**/*.*').on('change', browserSync.reload);
+});
+
+// Run sass, local-build, and serve
+gulp.task('default', gulp.series('local-build', 'sass', 'fonts', 'icons', 'serve'));
+
+// Pipe CNAME to _site if you are using a custom URL
+gulp.task('cname', function() {
+  return gulp.src(['CNAME'])
+    .pipe(gulp.dest('_site/'));
+});
+
+// Deploy _site to gh-pages; note: add the 'cname' task to this tasks series if you are using a custom URL
+gulp.task('deploy-gh-pages', function() {
+  return gulp.src('_site/**/*')
+    .pipe(deploy());
+});
+
+// Run production-build, and deploy-gh-pages
+gulp.task('deploy', gulp.series('production-build', 'sass', 'fonts', 'icons', 'deploy-gh-pages'));
---
+++ index.md
@@ -0,0 +1,86 @@
+---
+layout: default
+title:
+---
+
+{: class="f-2 u-pt-sm u-mb-md"}
+## Scale & Units
+
+{: class="f-3 u-mb-xl"}
+This IBM type scale is built on a mathematical progression that is arithmetic in nature, but still grows exponentially using multiples of a fraction with a power of 2 in the denominator, giving us more sensible values and line-heights for screen-based media.
+
+<div class="c-panel">
+  <div class="c-units">
+    <span class="f-5 c-type-label u-mb-xxs">16px • 1rem</span>
+    <div class="c-unit--16 u-mb-sm"></div>
+
+    <span class="f-5 c-type-label--disabled u-mb-xxs">No unit</span>
+    <div class="c-unit--20 u-mb-sm"></div>
+
+    <span class="f-5 c-type-label u-mb-xxs">24px • 1.5rem</span>
+    <div class="c-unit--24 u-mb-sm"></div>
+
+    <span class="f-5 c-type-label u-mb-xxs">32px • 2rem</span>
+    <div class="c-unit--md u-mb-sm"></div>
+
+    <span class="f-5 c-type-label u-mb-xxs">40px • 2.5rem</span>
+    <div class="c-unit--lg u-mb-sm"></div>
+
+    <span class="f-5 c-type-label u-mb-xxs">48px • 3rem</span>
+    <div class="c-unit--xl u-mb-sm"></div>
+
+    <span class="f-5 c-type-label u-mb-xxs">64px • 4rem</span>
+    <div class="c-unit--xxl u-mb-sm"></div>
+
+    <span class="f-5 c-type-label u-mb-xxs">80px • 5rem</span>
+    <div class="c-unit--jumbo u-mb-sm"></div>
+
+    <span class="f-5 c-type-label--disabled u-mb-xxs">No unit</span>
+  </div>
+  <div class="c-type-scale">
+    <span class="f-5 c-type-label u-mb-xxs">
+      12px/16px • 0.75rem/1.333333
+    </span>
+    <p class="f f-6 u-mb-sm">Plex 6</p>
+
+    <span class="f-5 c-type-label u-mb-xxs">
+      14px/20px • 0.875rem/1.428571
+    </span>
+    <p class="f f-5 u-mb-sm">Plex 5</p>
+
+    <span class="f-5 c-type-label u-mb-xxs">
+      16px/24px • 1rem/1.5
+    </span>
+    <p class="f f-4 u-mb-sm">Plex 4</p>
+
+    <span class="f-5 c-type-label u-mb-xxs">
+      20px/32px • 1.25rem/1.6
+    </span>
+    <h3 class="f f-3 u-mb-sm">Plex 3</h3>
+
+    <span class="f-5 c-type-label u-mb-xxs">
+      28px/40px • 1.75rem/1.428571
+    </span>
+    <h2 class="f f-2 u-mb-sm">Plex 2</h2>
+
+    <span class="f-5 c-type-label u-mb-xxs">
+      40px/48px • 2.5rem/1.2
+    </span>
+    <h1 class="f f-1 u-mb-sm">Plex 1</h1>
+
+    <span class="f-5 c-type-label u-mb-xxs">
+      56px/64px • 3.5rem/1.142857
+    </span>
+    <h1 class="f f-lg u-mb-sm">Plex LG</h1>
+
+    <span class="f-5 c-type-label u-mb-xxs">
+      76px/80px • 4.75rem/1.052632
+    </span>
+    <h1 class="f f-xl u-mb-sm">Plex XL</h1>
+
+    <span class="f-5 c-type-label u-mb-xxs">
+      100px/104 • 6.25rem/1.04
+    </span>
+    <h1 class="f f-xxl u-mb-sm">Plex XXL</h1>
+  </div>
+</div>
